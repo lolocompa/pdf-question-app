38bbdd700eb556219bbdd3f5bd474dc680279722
Lorenzo Comparetto <lolocomparetto@gmail.com>
2024-05-20 19:45:24 -0300

    implement input and header

--- frontend/pdf-question-app/src/App.js
+++ frontend/pdf-question-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./planetai.svg";
 import "./App.css";
 import uploadicon from "./upload.png";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
@@ -11,6 +12,15 @@ function App() {
           <img className="add-icon" src={uploadicon} alt="" />
           <h4>Upload PDF</h4>
         </div>
+      </div>
+      <div className="content"></div>
+      <div className="input_container">
+        <input
+          placeholder="Send a message..."
+          className="question"
+          type="text"
+        />
+        <i className="bi bi-send"></i>
       </div>
     </div>
   );
